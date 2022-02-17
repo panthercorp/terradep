1321ee05e834bae34e8b107a9e1bfd812358ef5b
Tejaswi Kasat <kasattejasvi@gmail.com>
2022-02-17 22:53:50 +0530

    added unit tests for file

--- pkg/files/file_test.go
+++ pkg/files/file_test.go
@@ -1,11 +1,19 @@
 package files
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
 
 func TestReadFile(t *testing.T) {
+	data := []byte("Hello World")
+	err := ioutil.WriteFile("./test_hello", data, 0755)
+	if err != nil {
+		t.Error("failed to initialize test")
+	}
 	type args struct {
 		filePath string
 	}
@@ -15,6 +23,22 @@ func TestReadFile(t *testing.T) {
 		want    []byte
 		wantErr bool
 	}{
+		{
+			name: "read file test if exists",
+			args: args{
+				filePath: "./test_hello",
+			},
+			want:    data,
+			wantErr: false,
+		},
+		{
+			name: "throw error if file does not exist",
+			args: args{
+				filePath: "./test_hello_not_exist",
+			},
+			want:    nil,
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -28,9 +52,14 @@ func TestReadFile(t *testing.T) {
 			}
 		})
 	}
+	err = os.Remove("./test_hello")
+	if err != nil {
+		t.Error("failed to cleanup test resources")
+	}
 }
 
 func TestWriteFile(t *testing.T) {
+	data := []byte("Hello World")
 	type args struct {
 		filePath   string
 		data       []byte
@@ -41,18 +70,39 @@ func TestWriteFile(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "write file test",
+			args: args{
+				filePath:   "./test_hello",
+				data:       data,
+				permission: 0755,
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := WriteFile(tt.args.filePath, tt.args.data, tt.args.permission); (err != nil) != tt.wantErr {
 				t.Errorf("WriteFile() error = %v, wantErr %v", err, tt.wantErr)
 			}
+			_, err := ioutil.ReadFile(tt.args.filePath)
+			if err != nil {
+				t.Error("failed to read file:", err)
+			}
 		})
+	}
+	err := os.Remove("./test_hello")
+	if err != nil {
+		t.Error("failed to cleanup test resources")
 	}
 }
 
 func TestCheckFileExistence(t *testing.T) {
+	data := []byte("Hello World")
+	err := ioutil.WriteFile("./test_hello", data, 0755)
+	if err != nil {
+		t.Error("failed to initialize test")
+	}
 	type args struct {
 		filePath string
 	}
@@ -61,7 +111,20 @@ func TestCheckFileExistence(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "check file existence test",
+			args: args{
+				filePath: "./test_hello",
+			},
+			want: true,
+		},
+		{
+			name: "check file existence test",
+			args: args{
+				filePath: "./test_hello_not_exist",
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -70,9 +133,18 @@ func TestCheckFileExistence(t *testing.T) {
 			}
 		})
 	}
+	err = os.Remove("./test_hello")
+	if err != nil {
+		t.Error("failed to cleanup test resources")
+	}
 }
 
 func TestCreateFileBackup(t *testing.T) {
+	data := []byte("Hello World")
+	err := ioutil.WriteFile("./test_hello", data, 0755)
+	if err != nil {
+		t.Error("failed to initialize test")
+	}
 	type args struct {
 		filePath   string
 		permission int
@@ -82,7 +154,22 @@ func TestCreateFileBackup(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "create file backup test",
+			args: args{
+				filePath:   "./test_hello",
+				permission: 0755,
+			},
+			wantErr: false,
+		},
+		{
+			name: "create file backup of non existing files test",
+			args: args{
+				filePath:   "./test_hello_not_exist",
+				permission: 0755,
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -91,9 +178,24 @@ func TestCreateFileBackup(t *testing.T) {
 			}
 		})
 	}
+	files, err := filepath.Glob("./test_hello*")
+	if err != nil {
+		t.Error("failed to get test generated files")
+	}
+	for _, file := range files {
+		err = os.Remove(file)
+		if err != nil {
+			t.Error("failed to cleanup test resources")
+		}
+	}
 }
 
 func TestDeleteExistingFile(t *testing.T) {
+	data := []byte("Hello World")
+	err := ioutil.WriteFile("./test_hello", data, 0755)
+	if err != nil {
+		t.Error("failed to initialize test")
+	}
 	type args struct {
 		filePath string
 	}
@@ -102,7 +204,20 @@ func TestDeleteExistingFile(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "delete existing file test",
+			args: args{
+				filePath: "./test_hello",
+			},
+			wantErr: false,
+		},
+		{
+			name: "delete non existing file test",
+			args: args{
+				filePath: "./test_hello_not_exist",
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -111,4 +226,12 @@ func TestDeleteExistingFile(t *testing.T) {
 			}
 		})
 	}
+	_, err = ioutil.ReadFile("./test_hello")
+	if err == nil {
+		err = os.Remove("./test_hello")
+		if err != nil {
+			t.Error("failed to cleanup test resources")
+		}
+	}
+
 }
