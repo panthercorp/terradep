d60c5105e1383653b61a9b8ba02773ae4ff4321a
Tejaswi Kasat <kasattejasvi@gmail.com>
2022-02-21 22:59:57 +0530

    Added code for initializing configs

--- cmd/config.go
+++ cmd/config.go
@@ -6,7 +6,10 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/panthercorp/terradep/internal/configs"
+	"github.com/panthercorp/terradep/pkg/files"
 	"github.com/spf13/cobra"
 )
 
@@ -14,17 +17,47 @@ import (
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Manage terradep configuration",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("config called")
-	},
 }
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "Initialize terradep configuration",
+	Short: "Initialize terradep/dcf configuration",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		userHomeDir, err := files.GetUserHomeDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+		typeFlag, err := cmd.Flags().GetString("type")
+		if err != nil {
+			log.Fatal(err)
+		}
+		pathFlag, err := cmd.Flags().GetString("path")
+		if err != nil {
+			log.Fatal(err)
+		}
+		forceFlag, err := cmd.Flags().GetBool("force")
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch typeFlag {
+		case "terradep":
+			var initializePath string
+			if pathFlag == "" {
+				initializePath = userHomeDir
+			} else {
+				initializePath = pathFlag
+			}
+			if err := configs.InitializeTerradepConfig(initializePath, forceFlag); err != nil {
+				log.Fatal(err)
+			}
+		case "dcf":
+			if err := configs.InitializeDcfConfig(pathFlag, forceFlag); err != nil {
+				log.Fatal(err)
+			}
+		default:
+			log.Fatal("Invalid config type")
+		}
 	},
 }
 
@@ -41,4 +74,8 @@ func init() {
 	rootCmd.AddCommand(configCmd)
 	configCmd.AddCommand(initCmd)
 	configCmd.AddCommand(repoCmd)
+
+	initCmd.Flags().BoolP("force", "f", false, "Force overwrite of existing terradep config")
+	initCmd.Flags().StringP("path", "p", "", "Path to terradep/dcf config")
+	initCmd.Flags().StringP("type", "t", "terradep", "Type of config - dcf or terradep")
 }
