e11926717fb1dfe099912019f8a55759ec5b4809
Tejaswi Kasat <kasattejasvi@gmail.com>
2022-02-27 15:24:17 +0530

    Fixed driver functions

--- internal/configs/driver.go
+++ internal/configs/driver.go
@@ -2,28 +2,27 @@ package configs
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 
+	"github.com/panthercorp/terradep/internal/statics"
 	"github.com/panthercorp/terradep/pkg/files"
 )
 
 // Path should be till folder only, not file
 // TODO fix relative path for copying files
 func InitializeTerradepConfig(path string, force bool) error {
-	terradepBase := filepath.Join("internal", "res", "terradepBase.yml")
 	destinationPath := filepath.Join(path, ".terradep", "terradep.yml")
+	os.Mkdir(filepath.Join(path, ".terradep"), 0755)
 	if files.CheckFileExistence(destinationPath) && !force {
 		return fmt.Errorf("terradep config already exists at %v. use --force to override", destinationPath)
 	} else if files.CheckFileExistence(destinationPath) && force {
-		if err := files.CreateFileBackup(destinationPath, 0644); err != nil {
+		if err := files.CreateFileBackup(destinationPath, 0755); err != nil {
 			return err
 		}
 	}
-	terradepConfigData, err := files.ReadFile(terradepBase)
-	if err != nil {
-		return err
-	}
-	if err := files.WriteFile(destinationPath, terradepConfigData, 0644); err != nil {
+	terradepConfigData := []byte(statics.TerradepConfig)
+	if err := files.WriteFile(destinationPath, terradepConfigData, 0755); err != nil {
 		return err
 	}
 	return nil
@@ -32,20 +31,16 @@ func InitializeTerradepConfig(path string, force bool) error {
 // Path should be till folder only, not file
 // TODO fix relative path for copying files
 func InitializeDcfConfig(path string, force bool) error {
-	dcfBase := filepath.Join("..", "res", "dcfBase.yml")
 	destinationPath := filepath.Join(path, "dcf.yml")
 	if files.CheckFileExistence(destinationPath) && !force {
 		return fmt.Errorf("dcf config already exists at %v. use --force to override", destinationPath)
 	} else if files.CheckFileExistence(destinationPath) && force {
-		if err := files.CreateFileBackup(destinationPath, 0644); err != nil {
+		if err := files.CreateFileBackup(destinationPath, 0755); err != nil {
 			return err
 		}
 	}
-	dcfConfigData, err := files.ReadFile(dcfBase)
-	if err != nil {
-		return err
-	}
-	if err := files.WriteFile(destinationPath, dcfConfigData, 0644); err != nil {
+	dcfConfigData := []byte(statics.DcfConfig)
+	if err := files.WriteFile(destinationPath, dcfConfigData, 0755); err != nil {
 		return err
 	}
 	return nil
